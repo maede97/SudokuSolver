8738c1f11c3ddecfc67e900a7a7c6550d3d3485d
Matthias Busenhart <busenham@student.ethz.ch>
2019-03-30 11:17:42 +0100

    Add first checks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SudokuSolver
 Solve Sudokus fast with c++.
+
+[![Documentation](https://codedocs.xyz/maede97/SudokuSolver.svg)](https://codedocs.xyz/maede97/SudokuSolver/)
--- sudokuSolver/src/base/SudokuBase.hpp
+++ sudokuSolver/src/base/SudokuBase.hpp
@@ -10,9 +10,9 @@
  */
 namespace Sudoku {
 
-using SudokuMatrix = Eigen::Matrix<short, 9, 9>;
-using idx_t = short;
-using val_t = short;
+using SudokuMatrix = Eigen::Matrix<short, 9, 9>; ///< internal type
+using idx_t = short; ///< index type
+using val_t = short; ///< value type
 
 /**
  * @brief Internal Sudoku representation
@@ -23,9 +23,59 @@ public:
     Sudoku() {
         _matrix = SudokuMatrix::Zero();
     }
+    /**
+     * @brief add a new entry
+     * @param row index of row
+     * @param col index of column
+     * @param value number at this position
+     */
+    void addEntry(idx_t row, idx_t col, val_t value) {
+        checkValueExist(row, col);
+        checkRange(value);
+        _matrix(row, col) = value;
+        check();
+    }
+
+    /**
+     * @brief remove entry at position
+     * @param row index of row
+     * @param col index of column
+     */ 
+    void removeEntry(idx_t row, idx_t col) {
+        _matrix(row, col) = 0;
+    }
 
 private:
-    SudokuMatrix _matrix;
+    /**
+     * @brief check if all sudoku constraints hold
+     */
+    void check() {
+
+    }
+
+    /**
+     * @brief check if value is in bounds (1-9)
+     * @param value value to check
+     */
+    void checkRange(val_t value) {
+        if(value < 1 || value > 9) {
+            throw "Value " + std::to_string(value) + " out of bounds";
+        }
+    }
+
+    /**
+     * @brief check if position is empty
+     * @param row index of row
+     * @param col index of column
+     */
+    void checkValueExist(idx_t row, idx_t col) {
+        if(_matrix(row, col)!= 0) {
+            throw "Sudoku not empty at " + std::to_string(row) + "/" + std::to_string(col);
+        }
+    }
+
+    SudokuMatrix _matrix; ///< internal representation of sudoku
+
 };
 
 } /* namespace Sudoku */
